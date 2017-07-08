7d580143916ac6c2df738231cd9001e23a9d5e59
marcelo_bandeira <marcelob100@hotmail.com>
2017-07-08 15:17:37 -0300

    calc percent votes using @property.

--- pool/models.py
+++ pool/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.db.models import Sum
 
 # Create your models here.
 class Question(models.Model):
@@ -13,3 +14,12 @@ class Choice(models.Model):
 	question = models.ForeignKey(Question, null=True, blank=True, on_delete=models.CASCADE, related_name='choices')
 	choice_text = models.CharField(max_length=500)
 	votes = models.IntegerField(default=0)
+
+	@property
+	def percent_prop(self):
+
+		total_votes = self.question.choices.aggregate(soma = Sum('votes'))
+		
+		percent = float(self.votes) / total_votes['soma'] * 100
+
+		return percent
